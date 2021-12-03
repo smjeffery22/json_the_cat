156f97f341927c7e841e847ea15295532cd40a01
Jeffery <parkjon8@gmail.com>
2021-12-03 01:05:02 +0000

    Refactored breedFetcher.js to export function and added index.js for all of the user-facing (terminal output) logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,41 @@
 const request = require("request");
 
-const breedName = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
 
-request(url, (error, response, body) => {
-  // Error
-  const error404 = `{"message":"404 - please consult the documentation for correct url to call. https://docs.thecatapi.com/"}`
 
-  if (error) {
-    console.log('Error!\n', error);
-    return;
-  }
-
-  if (body === error404) {
-    console.log('Error!\n', error404);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   
-  // Parse the content into object
-  const data = JSON.parse(body);
-
-  // Breed Not Found
-  if (data.length === 0) {
-    console.log("Such breed does not exist. Try again.");
-    return;
-  }
+  request(url, (error, response, body) => {
+    // Error
+    // const error404 = `{"message":"404 - please consult the documentation for correct url to call. https://docs.thecatapi.com/"}`;
+    
+    if (error) {
+      return callback(error);
+      // console.log('Error!\n', error);
+      // return;
+    }
+     
+    // if (body === error404) {
+      
+    //   console.log('Error!\n', error404);
+    //   return;
+    // }
+    
+    // Parse the content into object
+    const data = JSON.parse(body);
   
-  // Prints the description of the breed
-  console.log('description:', data[0]['description']);
-});
+    // Breed Not Found
+    if (data.length === 0) {
+      // When there is a successful response, error is null so null needs to be included as the first argument since the callback function from index.js requires error as the first argument
+      return callback(null, 'Breed not found');
+      // console.log("Such breed does not exist. Try again.");
+      // return;
+    }
+    
+    // Prints the description of the breed
+    return callback(null, data[0]['description']);
+    // console.log('description:', data[0]['description']);
+  });
+};
+
+module.exports = { fetchBreedDescription };
